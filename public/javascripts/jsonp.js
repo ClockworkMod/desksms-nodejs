87466f7ca9769edaa05ceff17d30ed3e806a1d75
Koushik Dutta <koushd@gmail.com>
2011-08-23 23:39:20 -0700

    xhr

--- public/javascripts/jsonp.js
+++ public/javascripts/jsonp.js
@@ -1,9 +1,17 @@
 var jsonp = function(url, cb, data) {
+  var type = "jsonp";
+
+  if (url.indexOf("https://desksms.appspot.com") == 0 || url.indexOf("http://desksms.appspot.com") == 0)
+    type = "json";
+
+  if (url.indexOf("https://2.desksms.appspot.com") == 0 || url.indexOf("http://2.desksms.appspot.com") == 0)
+    type = "json";
+
   var jqXHR = $.get(url, data, function(data) {
     if (cb)
       cb(null, data);
   },
-  "jsonp").error(function(err) {
+  type).error(function(err) {
     if (cb)
       cb(err);
   }).complete(function(jqXHR) {
